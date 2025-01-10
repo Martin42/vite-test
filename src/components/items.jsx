f4653769658a4d6268dc02e30a436c4b342b3642
Martin42 <web.martin.dev@gmail.com>
2025-01-10 17:42:10 +0000

    -

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,29 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import itemsJSON from "../items.json";
 
 export const Items = () => {
   const [items, setItems] = useState(itemsJSON);
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [sort, setSort] = useState("⌄");
+
+  // TODO: Merge both sort and search in the same function.
+  // The onChanges and onClick should be just to change the states
 
   const handleItemSearch = (e) => {
     const searchValue = e.target.value;
-    const searchedItems = itemsJSON.filter((element) =>
-      element.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
-    setItems(searchedItems);
+
+    if (categoryFilter !== "") {
+      const searchedItems = itemsJSON.filter(
+        (element) =>
+          element.name.toLowerCase().includes(searchValue.toLowerCase()) &&
+          element.category === categoryFilter
+      );
+      setItems(searchedItems);
+    } else {
+      const searchedItems = itemsJSON.filter((element) =>
+        element.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setItems(searchedItems);
+    }
     console.log(items);
   };
 
   const handleCategoryFilter = (e) => {
     const filter = e.target.value;
+    setCategoryFilter(filter);
     console.log(e.target.value);
 
     if (filter === "") {
       setItems(itemsJSON);
     } else {
-      const filteredCategoryItems = itemsJSON.filter(
+      const filteredCategoryItems = items.filter(
         (item) => item.category === filter
       );
       setItems(filteredCategoryItems);
+    }
+  };
+
+  const handleSort = () => {
+    if (sort === "⌃") {
+      setSort("⌄");
+      const sortedItems = items.sort((a, b) => b.price - a.price);
+      setItems(sortedItems);
+    } else {
+      setSort("⌃");
+      const sortedItems = items.sort((a, b) => a.price - b.price);
+      setItems(sortedItems);
     }
   };
 
@@ -40,6 +68,11 @@ export const Items = () => {
     ));
   };
 
+  useEffect(() => {
+    const sortedItems = itemsJSON.sort((a, b) => b.price - a.price);
+    setItems(sortedItems);
+  }, []);
+
   return (
     <div>
       <input type="text" placeholder="search..." onChange={handleItemSearch} />
@@ -47,6 +80,9 @@ export const Items = () => {
         <option value="">Choose a category</option>
         {categoryDropdown()}
       </select>
+      <button type="button" onClick={handleSort}>
+        Sort Price {sort}
+      </button>
       <table>
         <tr>
           <th>Name</th>
